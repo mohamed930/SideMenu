2b963f93f31ae4d16bf1120c4ea062ed3cdb1ac9
Mohamed Ali <mo.ali12477@gmail.com>
2021-06-14 13:59:59 -0700

    Add HideMenu without Animation Commit!

--- SideMenu/Controllers/MainViewController.swift
+++ SideMenu/Controllers/MainViewController.swift
@@ -9,8 +9,8 @@ import UIKit
 
 class MainViewController: UIViewController {
     
-//    @IBOutlet weak var SideMenuLeading: NSLayoutConstraint!
-//    @IBOutlet weak var BackgroundSideMenu: UIView!
+    @IBOutlet weak var BackgroundSideMenuLeading: NSLayoutConstraint!
+    @IBOutlet weak var BackgroundSideMenu: UIView!
     @IBOutlet weak var SideMenuView: UIView!
     @IBOutlet weak var SideMenuLeading: NSLayoutConstraint!
 
@@ -21,18 +21,33 @@ class MainViewController: UIViewController {
     }
     
     private func LoadPage() {
-        SideMenuLeading.constant = 412
-        SideMenuView.isHidden = true
+//        SideMenuLeading.constant = 412
+//        SideMenuView.isHidden = true
+        BackgroundSideMenu.isHidden = true
+        BackgroundSideMenuLeading.constant = 414
     }
     
     @IBAction func SideMenuButton (_ sender:Any) {
         ShowMenu()
     }
     
+    
+    @IBAction func TapGeusterTapped(_ sender: Any) {
+        HideMenu()
+    }
+    
     private func ShowMenu() {
         print("Side Menu Presented")
-        SideMenuView.isHidden = false
-        SideMenuLeading.constant = 108
+//        SideMenuView.isHidden = false
+//        SideMenuLeading.constant = 108
+        BackgroundSideMenu.isHidden = false
+        BackgroundSideMenuLeading.constant = 0
+        //SideMenuLeading.constant = 88
+    }
+    
+    private func HideMenu() {
+        print("Side Menu Hidden")
+        LoadPage()
     }
 
 
